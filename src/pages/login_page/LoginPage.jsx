043956bbc9f1e5b55ca391011a84eb035e234968
subhajit-projects <subhajitdey.5198@gmail.com>
2024-06-09 09:45:10 +0530

    Revert "Merge branch 'dev/env' into develop"
    This reverts commit 138cc719bcd9a4fa0e7031b33665f6a7c6cc8262, reversing
    changes made to 7f1477e5eb3cbb57055d8f37c6d16a7b4e3a5ad4.

--- src/pages/login_page/LoginPage.jsx
+++ src/pages/login_page/LoginPage.jsx
@@ -37,12 +37,11 @@ const LoginPage = () => {
     }
 
     useEffect(() => {
-        
+
     }, [userDetails])
 
     return (
         <Container  className={style.loginFormSpace}>
-            {console.log(process.env.REACT_APP_API_BASE_URL)}
             <h5>Main Admin panel</h5>
             <div className={style.welcome}>
                 <h6>Welcome! Admin.</h6>
