1feb54d8bd53d4f6673d25c32ef2675d2fed3adc
subhajit-projects <subhajitdey.5198@gmail.com>
2024-06-12 07:25:28 +0530

    Add new resux action for display name

--- src/myredux/reducers/index.js
+++ src/myredux/reducers/index.js
@@ -1,10 +1,12 @@
 import sidebarReducer from './sidebar';
 import CurrentRouterReducer from './CurrentRouter';
 import { combineReducers } from 'redux';
+import NavbarContent from './NavbarContent';
 
 const allReducers = combineReducers({
     sidebar: sidebarReducer,
     currentRoute: CurrentRouterReducer,
+    navbarContent: NavbarContent
 })
 
 export default allReducers;
